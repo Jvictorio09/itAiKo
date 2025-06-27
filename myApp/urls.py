b613420def79aee12f56a3aefca35cf1a257450c
Jvictorio09 <juliavictorio16@gmail.com>
2025-06-27 16:08:31 +0800

    updated

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('webhook/', views.webhook, name='messenger_webhook'),
-    #path('signup/', views.signup_view, name='signup'),
-    #path('login/', views.login_view, name='login'),
-    #path('logout/', views.custom_logout_view, name='logout'),
-    #path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('webhook/', views.webhook, name='messenger_webhook'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.custom_logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 
 ]
